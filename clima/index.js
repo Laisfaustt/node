0366527204d210096f7d90800bba5b8cf59887c9
Laisfaustt <lais.s.faustino@gmail.com>
2025-04-30 10:14:26 -0300

    l

---
+++ clima/index.js
@@ -0,0 +1,26 @@
+// Importando as bibliotecas
+const express = require("express")
+const axios = require("axios")
+require("dotenv") .config()
+// Configurações do servidor
+const porta = 3000
+const app = express ()
+
+app.use(express.static('public'))
+
+app.get("/api/clima", async (requisicao, resposta) => {
+    const cidade = requisicao.query.cidade
+    const chaveAPI = process.env.OPENWEATHER_API_KEY
+    try {
+        const respostaAPI = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chaveAPI}&units=metric&lang=pt_br`)
+        resposta.json(respostaAPI.data)
+    }
+    catch {
+        resposta.status(500).json({erro: "Não foi possivel buscar o clima"})
+    }
+
+    })
+    
+app.listen(porta, () => {
+    console.log(`Servidor Rodando em: http:localhost:${porta}`)
+})
---
+++ clima/public/script.js
@@ -0,0 +1,36 @@
+// Garante que o JS so vai executar depois que todo HTML tiver carregado
+document. addEventListener("DOMContentLoaded", () => {
+    // Pegando os elementos do HTML
+    const campoCidade = document. getElementById("cidade")
+    const botaoBuscar = document. getElementById("buscar")
+    const nomeCidade = document.getElementById("nome-cidade")
+    const temperatura = document.getElementById("temperatura")
+    const umidade = document. getElementById("umidade")
+    const vento = document.getElementById("vento")
+    const descricao = document. getElementById("descricao")
+
+// Adicionando evento ao clicar no botao
+    botaoBuscar. addEventListener("click", buscarClima)
+// Funcao que busca o clima
+    async function buscarClima() {
+        const cidade = campoCidade.value.trim()
+        if(!cidade){
+            alert("Por favor, Digite uma cidade")
+            return
+        }
+        try{
+            const resposta = await fetch(`/api/clima?cidade=${cidade}`)
+            const dados = await resposta.json()
+            // Atualizando os dados no site
+            nomeCidade. textContent = dados.name
+            temperatura. textContent = Math. round (dados.main. temp)
+            umidade. textContent = dados.main. humidity
+            vento. textContent = Math. round(dados.wind. speed * 3.6)
+            descricao. textContent = dados.weather [0].description
+        }   
+        catch(erro){
+            alert("nao foi possivel buscar essa cidade")
+        }
+    }
+
+})
