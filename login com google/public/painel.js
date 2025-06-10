86e638c10ae9e370941c9e42d9a12e392734ec05
Laisfaustt <lais.s.faustino@gmail.com>
2025-06-10 16:01:27 -0300

    .
    .

---
+++ login com google/public/painel.js
@@ -0,0 +1,41 @@
+fetch('api/usuario')
+    .then(resposta => resposta. json() )
+    .then (usuario => {
+
+        document.getElementById("nome-usuario") . textContent =
+usuario.displayName
+        document.getElementById("email-usuario") . textContent =
+usuario.emails[0].value
+    })
+    .catch(erro => {
+        console.error("Erro ao buscar dados do usuario", erro)
+        window. location.href = '/'
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- modolu os/script.js
+++
@@ -1,6 +0,0 @@
-const os = require("os")
-
-console.log("sistema operacional:", os.platform())
-console.log("arquitetura:", os.arch())
-console.log("memoria livre:", os.freemem(), "bytes")
-console.log("memoria livre:", os.totalmem(), "bytes")
